307decf0053ac290cebffdb9de8e34aa310916ae
Geoff Hackworth <geoff@hacknicity.com>
2020-02-10 17:11:55 +0000

    Changed project iOS Deployment Target to iOS 11, used availability checks to fix build errors, and added privacy protection logic to the App Delegate.

--- AppSwitcherPrivacyScreenObjC/AppDelegate.m
+++ AppSwitcherPrivacyScreenObjC/AppDelegate.m
@@ -8,7 +8,11 @@
 
 #import "AppDelegate.h"
 
+#import "PrivacyProtectionViewController.h"
+
 @interface AppDelegate ()
+
+@property (strong, nullable, nonatomic) UIWindow *privacyProtectionWindow;
 
 @end
 
@@ -21,21 +25,48 @@
 }
 
 
+#pragma mark - UIApplication lifecycle
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [self hidePrivacyProtectionWindow];
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    [self showPrivacyProtectionWindow];
+}
+
+
 #pragma mark - UISceneSession lifecycle
 
 
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options  API_AVAILABLE(ios(13.0)) {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
 
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions API_AVAILABLE(ios(13.0)) {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
+
+// MARK: Privacy Protection
+
+- (void)showPrivacyProtectionWindow {
+    self.privacyProtectionWindow = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+
+    self.privacyProtectionWindow.rootViewController = [[PrivacyProtectionViewController alloc] init];
+    self.privacyProtectionWindow.windowLevel = UIWindowLevelAlert + 1;
+
+    [self.privacyProtectionWindow makeKeyAndVisible];
+}
+
+- (void)hidePrivacyProtectionWindow {
+    self.privacyProtectionWindow.hidden = YES;
+    self.privacyProtectionWindow = nil;
+}
 
 @end
